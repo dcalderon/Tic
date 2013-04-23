23483b7b2235cfcd3a227049e0d45b43a886c6cb
Danny Sullivan <danielalexandersullivan@gmail.com>
2013-04-23 16:15:05 -0400

    added tictac images, first commit

--- app/view/FirstImageTouch.js
+++ app/view/FirstImageTouch.js
@@ -3,12 +3,12 @@ Ext.define('Tic.view.FirstImageTouch', {
     xtype: 'toucher',
     config: {
         title: null,
-        src: 'http://src.sencha.io/740/500/http://diegocalderon.info/travels/wp-content/uploads/2012/07/Prague43.jpg',
+        src: 'resources/images/blank.jpg',
         description: null,
         count: 0,
-        choices: ['http://src.sencha.io/740/500/http://diegocalderon.info/travels/wp-content/uploads/2012/07/Prague43.jpg',
-                    'http://src.sencha.io/740/500/http://diegocalderon.info/travels/wp-content/uploads/2012/07/Berlin15.jpg',
-                    'http://src.sencha.io/740/500/http://diegocalderon.info/travels/wp-content/uploads/2012/08/Brussels40.jpg']
+        choices: ['resources/images/x.jpg',
+                    'resources/images/o.jpg',
+                    'resources/images/blank.jpg']
     },
 
     //sets up our tap event listener
